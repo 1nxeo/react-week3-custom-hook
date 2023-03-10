de8ddb5c2079dfe9435352573507ddef1373ccb4
Inseo Yang <y.1nseooo@gmail.com>
2023-03-10 16:24:34 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import useInput from "./hooks/useInput";
+
+function App() {
+  const [name, onChangeNameHandler] = useInput();
+  const [password, onChangePasswordHandler] = useInput();
+
+  // const [name, setName] = useState("");
+  // const [password, setPassword] = useState("");
+
+  // const onChangeNameHandler = (e) =>{
+  //   setName(e.target.value)
+  // }
+
+  // const onChangePasswordHandler = (e) =>{
+  //   setPassword(e.target.value)
+  // }
+
+  return (
+    <div>
+      <input type="text" value={name} onChange={onChangeNameHandler} />
+      <input type="text" value={password} onChange={onChangePasswordHandler} />
+    </div>
+  );
+}
+
+export default App;
